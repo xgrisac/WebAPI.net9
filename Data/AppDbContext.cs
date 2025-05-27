6a56b92716757cf86da2407466b17e18e7bc198b
Isac Ribeiro <isackolling122@gmail.com>
2025-05-26 21:06:45 -0300

    First Commit. Criação e configuração do ambiente, configuração de rota do DB

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore; // Responsável por ciar a conexão, enviar e coletar dados do banco de dados
+using WebAPI.net9.Models; 
+
+namespace WebAPI.net9.Data 
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options) // Construtor que recebe as opções de configuração do DbContext
+        {
+        }
+
+        // Criação da tabela Produtos com as confgs ProdutoModel no banco de dados
+        public DbSet<ProdutoModel> Produtos { get; set; } // DbSet é uma coleção de entidades que podem ser consultadas e manipuladas
+
+    }
+}
---
+++ Models/ProdutoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI.net9.Models
+{
+    public class ProdutoModel
+    {   // Propriedades que permitem a criação e alteração do produto
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public int QuantidadeEstoque { get; set; }
+        public string CodigoDeBarras { get; set; } = string.Empty;
+        public string Marca { get; set; } = string.Empty;
+
+    }
+}
